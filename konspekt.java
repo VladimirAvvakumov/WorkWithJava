fb4ee02e929ce68920266b018dbffe3465346cc3
Vadimir Avvakumov <Birmengeem@bk.ru>
2024-07-19 12:49:53 +0300

    создал конспект

---
+++ konspekt.java
@@ -0,0 +1,11 @@
+/** коментарии слэш звездочка или просто звёздочка или как в C# // тоже коментарий.
+ * programm
+ */
+public class konspekt {
+    public static void main(String[] args) {
+        System.out.println("bye world");
+    }   
+}
+
+
+
